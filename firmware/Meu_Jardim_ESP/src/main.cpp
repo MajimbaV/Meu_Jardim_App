e28bb8acae3e916744a4b87649317c78e0fa141e
MichelZero <silva.michel@ifpb.edu.br>
2026-06-27 09:22:15 -0300

    up

--- firmware/Meu_Jardim_ESP/src/main.cpp
+++ firmware/Meu_Jardim_ESP/src/main.cpp
@@ -1,18 +1,29 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// O pino 2 é onde fica o LED azul embutido na placa da maioria dos ESP8266
+#define PINO_LED 2 
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Inicia o monitor serial na mesma velocidade que você configurou no .ini
+  Serial.begin(115200);
+  
+  // Dá um pequeno tempo para o serial estabilizar
+  delay(1000); 
+  
+  // Configura o pino do LED como saída (a lógica será a mesma para o relê depois)
+  pinMode(PINO_LED, OUTPUT);
+  
+  Serial.println("\n--- Sistema Meu Jardim Iniciado! ---");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // No ESP8266, o estado LOW (baixo) geralmente LIGA o LED embutido
+  digitalWrite(PINO_LED, LOW);
+  Serial.println("Rega LIGADA");
+  delay(2000); // Fica ligado por 2 segundos
+  
+  // O estado HIGH (alto) DESLIGA o LED embutido
+  digitalWrite(PINO_LED, HIGH);
+  Serial.println("Rega DESLIGADA");
+  delay(2000); // Fica desligado por 2 segundos
 }
